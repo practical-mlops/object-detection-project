0dc98105a012c6f2f6d69ca226a4ad2ff3466e97
Benjamin Tan <benjamintanweihao@gmail.com>
2024-01-17 01:22:32 +0800

    Working version of BentoML service

--- serving/service.py
+++ serving/service.py
@@ -1,48 +1,55 @@
+import json
+import shutil
+
 import PIL
 import bentoml
 from ultralytics import YOLO
 from bentoml.io import Image
-from bentoml.io import PandasDataFrame
+from bentoml.io import JSON
+import os
 
 
 class YOLOv8Runnable(bentoml.Runnable):
     SUPPORTED_RESOURCES = ("cpu",)
-    SUPPORTS_CPU_MULTI_THREADING = False
+    SUPPORTS_CPU_MULTI_THREADING = True
 
     def __init__(self):
-        # TODO: How can we pass in the path here?
         self.model = YOLO("yolov8_custom.pt")
+        self.model.cpu()
 
-    @bentoml.Runnable.method(batchable=False, batch_dim=0)
+    @bentoml.Runnable.method(batchable=False)
     def inference(self, input_imgs):
-        # Return predictions only
-        results = self.model(input_imgs)
-        return results.pandas().xyxy
-
-    @bentoml.Runnable.method(batchable=False, batch_dim=0)
-    def render(self, input_imgs):
         # Return images with boxes and labels
         results = self.model(input_imgs)[0]
 
-        im_array = results.plot()  # plot a BGR numpy array of predictions
-        im = PIL.Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-        im.save('results.jpg')
-        return im
+        return json.loads(results[0].tojson())
+
+    @bentoml.Runnable.method(batchable=False)
+    def render(self, input_imgs):
+
+        predict_path = os.path.join(os.getcwd(), "predict")
+        image_path = os.path.join(predict_path, "image0.jpg")
+
+        if os.path.exists(predict_path) and os.path.isdir(predict_path):
+            shutil.rmtree(predict_path)
+
+        _ = self.model(input_imgs, save=True, project=os.getcwd())
+
+        return PIL.Image.open(image_path)
 
 
-
-yolo_v8_runner = bentoml.Runner(YOLOv8Runnable, max_batch_size=30)
+yolo_v8_runner = bentoml.Runner(YOLOv8Runnable)
 
 svc = bentoml.Service("yolo_v8", runners=[yolo_v8_runner])
 
 
-@svc.api(input=Image(), output=PandasDataFrame())
+@svc.api(input=Image(), output=JSON())
 async def invocation(input_img):
-    batch_ret = await yolo_v8_runner.inference.async_run([input_img])
-    return batch_ret[0]
+    ret = await yolo_v8_runner.inference.async_run([input_img])
+    return ret
 
 
 @svc.api(input=Image(), output=Image())
 async def render(input_img):
-    batch_ret = await yolo_v8_runner.render.async_run([input_img])
-    return batch_ret[0]
+    ret = await yolo_v8_runner.render.async_run([input_img])
+    return ret
